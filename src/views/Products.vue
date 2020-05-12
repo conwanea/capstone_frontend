1dffc152f591bed669b74adda2fd851dd0ef53f9
Neal Conway <conwanea@gmail.com>
2020-05-11 19:10:26 -0500

    uploading before installing a theme

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -2,7 +2,15 @@
   <div class="products">
     <h1>All Products</h1>
     <div v-for="plate in plates">
-      <h2>{{ plate }}</h2>
+      <h2>{{ plate.print_name }}</h2>
+      <h2>{{ plate.scientific_name }}</h2>
+      <h3>Plate number: {{ plate.plate_number }}</h3>
+      <h3>{{ plate.medium }}</h3>
+      <h3>{{ plate.price }}</h3>
+      <h3>{{ plate.number_of_prints }}</h3>
+      <p>{{ plate.description }}</p>
+      <img v-bind:src="plate.image_url" width="300" height="400" />
+      <br />________________________________________________________
     </div>
   </div>
 </template>
